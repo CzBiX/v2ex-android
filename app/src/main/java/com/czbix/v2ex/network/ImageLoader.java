84b551ccb4b9334f26b840ccf54dbfc8f20754d4
CzBiX <gliuwr@gmail.com>
2015-05-26 17:36:13 +0800

    adjust ui

--- app/src/main/java/com/czbix/v2ex/network/ImageLoader.java
+++ app/src/main/java/com/czbix/v2ex/network/ImageLoader.java
@@ -3,13 +3,17 @@ package com.czbix.v2ex.network;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.RemoteException;
+import android.support.annotation.Nullable;
 import android.util.Log;
 import android.widget.ImageView;
 
 import com.czbix.v2ex.common.exception.ConnectionException;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class ImageLoader {
     private static final ImageLoader INSTANCE;
@@ -28,41 +32,56 @@ public class ImageLoader {
         mExecutorService = Executors.newCachedThreadPool();
     }
 
-    public void add(ImageView view, String url) {
-        mExecutorService.submit(new ImageLoadTask(view, url));
+    public Future<?> add(int taskId, ImageView view, String url, Callback callback) {
+        return mExecutorService.submit(new ImageLoadTask(taskId, view, url, callback));
     }
 
     private static class ImageLoadTask implements Runnable {
         private static final String TAG = ImageLoadTask.class.getSimpleName();
 
+        private final int mTaskId;
         private final ImageView mView;
         private final String mUrl;
+        private final Callback mCallback;
 
-        public ImageLoadTask(ImageView view, String url) {
-            this.mView = view;
-            this.mUrl = url;
+        public ImageLoadTask(int taskId, ImageView view, String url, Callback callback) {
+            Preconditions.checkNotNull(view);
+            Preconditions.checkArgument(!Strings.isNullOrEmpty(url));
+            Preconditions.checkNotNull(callback);
+
+            mTaskId = taskId;
+            mView = view;
+            mUrl = url;
+            mCallback = callback;
         }
 
         @Override
         public void run() {
-            final byte[] bytes;
+            byte[] bytes;
             try {
                 bytes = RequestHelper.getImage(mUrl);
             } catch (ConnectionException | RemoteException e) {
                 Log.w(TAG, "download image failed", e);
-                return;
+                bytes = null;
             }
 
-            final Bitmap src = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
-            final int size = mView.getHeight();
-            final Bitmap dst = Bitmap.createScaledBitmap(src, size, size, false);
+            final Bitmap result;
+            if (bytes == null) {
+                result = null;
+            } else {
+                final Bitmap src = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                final int size = mView.getHeight();
+                result = Bitmap.createScaledBitmap(src, size, size, false);
+            }
 
-            mView.post(new Runnable() {
-                @Override
-                public void run() {
-                    mView.setImageBitmap(dst);
-                }
-            });
+            mCallback.onImgLoadFinish(mTaskId, result);
         }
+    }
+
+    public interface Callback {
+        /***
+         * it's not called on UI thread.
+         */
+        void onImgLoadFinish(int taskId, @Nullable Bitmap bitmap);
     }
 }
--- app/src/main/java/com/czbix/v2ex/ui/adapter/TopicAdapter.java
+++ app/src/main/java/com/czbix/v2ex/ui/adapter/TopicAdapter.java
@@ -1,6 +1,8 @@
 package com.czbix.v2ex.ui.adapter;
 
+import android.graphics.Bitmap;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -46,13 +48,14 @@ public class TopicAdapter extends RecyclerView.Adapter<TopicAdapter.ViewHolder> 
         return mData == null ? 0 : mData.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, ImageLoader.Callback {
         public final TextView mTitle;
         public final ImageView mAvatar;
         public final TextView mUsername;
         public final TextView mNode;
         public final TextView mReplyCount;
         public final TextView mTime;
+        private int mId;
 
         public ViewHolder(View view) {
             super(view);
@@ -68,14 +71,22 @@ public class TopicAdapter extends RecyclerView.Adapter<TopicAdapter.ViewHolder> 
         }
 
         public void fillData(Topic topic) {
+            if (mId == topic.getId()) {
+                return;
+            }
+
+            mId = topic.getId();
+
             mTitle.setText(topic.getTitle());
             mUsername.setText(topic.getMember().getUsername());
-            mNode.setText(topic.getNode().getTitle());
+            mNode.setText("› " + topic.getNode().getTitle());
             mTime.setText(topic.getReplyTime());
             mReplyCount.setText(Integer.toString(topic.getReplyCount()));
 
+            mAvatar.setImageBitmap(null);
             final Avatar avatar = topic.getMember().getAvatar();
-            ImageLoader.getInstance().add(mAvatar, avatar.getUrlByDp(mAvatar.getHeight()));
+            ImageLoader.getInstance().add(mId, mAvatar,
+                    avatar.getUrlByDp(32), this);
         }
 
         @Override
@@ -84,6 +95,21 @@ public class TopicAdapter extends RecyclerView.Adapter<TopicAdapter.ViewHolder> 
             if (v == itemView) {
                 mListener.onItemClick(position, mData.get(position));
             }
+        }
+
+        @Override
+        public void onImgLoadFinish(final int taskId, @Nullable final Bitmap bitmap) {
+            if (taskId != mId || bitmap == null) {
+                return;
+            }
+
+            mAvatar.post(new Runnable() {
+                @Override
+                public void run() {
+                    if (taskId != mId) return;
+                    mAvatar.setImageBitmap(bitmap);
+                }
+            });
         }
     }
 
