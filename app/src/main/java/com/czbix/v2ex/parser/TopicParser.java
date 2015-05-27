1d7d4e10db06ee7f76c0e2ac809b42fc917d3745
CzBiX <gliuwr@gmail.com>
2015-05-27 21:22:18 +0800

    fix bug when topic have not content

--- app/src/main/java/com/czbix/v2ex/parser/TopicParser.java
+++ app/src/main/java/com/czbix/v2ex/parser/TopicParser.java
@@ -6,18 +6,21 @@ import com.google.common.collect.Lists;
 
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class TopicParser {
     public static TopicWithComments parseDoc(Document doc, Topic topic) {
         final Topic.Builder topicBuilder = topic.toBuilder();
 
-        parseContent(topicBuilder, doc.select(".topic_content").get(0));
+        parseContent(topicBuilder, doc.select(".topic_content"));
 
         // TODO:
         return new TopicWithComments(topicBuilder.createTopic(), Lists.<String>newArrayList());
     }
 
-    private static void parseContent(Topic.Builder builder, Element ele) {
+    private static void parseContent(Topic.Builder builder, Elements elements) {
+        if (elements.size() != 1) return;
+        final Element ele = elements.get(0);
         builder.setContent(ele.html());
     }
 }
