274e7b583af8ca59496328eb4fac21c3a0e71f41
CzBiX <gliuwr@gmail.com>
2015-06-01 09:58:55 +0800

    update reply count when view topic

--- app/src/main/java/com/czbix/v2ex/parser/TopicParser.java
+++ app/src/main/java/com/czbix/v2ex/parser/TopicParser.java
@@ -17,15 +17,26 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TopicParser extends Parser {
-    private static final Pattern PATTERN_THANKS = Pattern.compile("\\d+");
+    private static final Pattern PATTERN_NUMBERS = Pattern.compile("\\d+");
 
     public static TopicWithComments parseDoc(Document doc, Topic topic) {
         final Topic.Builder topicBuilder = topic.toBuilder();
 
         parseContent(topicBuilder, doc.select(".topic_content"));
+        parseTopicInfo(topicBuilder, doc);
         List<Comment> comments = parseComments(doc.select("#Main > div:nth-child(4) tr"));
 
         return new TopicWithComments(topicBuilder.createTopic(), comments);
+    }
+
+    private static void parseTopicInfo(Topic.Builder topicBuilder, Document doc) {
+        final Elements elements = doc.select("#Main > div:nth-child(4) > .cell:nth-child(1) > span");
+        Preconditions.checkState(elements.size() == 1);
+        final String text = elements.get(0).ownText();
+        final Matcher matcher = PATTERN_NUMBERS.matcher(text);
+        Preconditions.checkState(matcher.find());
+
+        topicBuilder.setReplyCount(Integer.parseInt(matcher.group()));
     }
 
     private static List<Comment> parseComments(Elements elements) {
@@ -72,7 +83,7 @@ public class TopicParser extends Parser {
         builder.setReplyTime(timeEle.text());
 
         if (elements.size() == 2) {
-            final Matcher matcher = PATTERN_THANKS.matcher(elements.get(1).text());
+            final Matcher matcher = PATTERN_NUMBERS.matcher(elements.get(1).text());
             Preconditions.checkState(matcher.find());
 
             final int thanks = Integer.parseInt(matcher.group());
