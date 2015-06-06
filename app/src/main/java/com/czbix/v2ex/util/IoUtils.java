eb004ac6116ef30bc8d2a2078593ea64629a30e3
CzBiX <gliuwr@gmail.com>
2015-06-06 13:54:59 +0800

    refine code

--- app/src/main/java/com/czbix/v2ex/util/IoUtils.java
+++ app/src/main/java/com/czbix/v2ex/util/IoUtils.java
@@ -5,11 +5,13 @@ import com.czbix.v2ex.AppCtx;
 import java.io.File;
 
 public class IoUtils {
-    public static File getCachePath() {
-        return AppCtx.getInstance().getCacheDir();
+    private static File cacheDir;
+
+    static {
+        cacheDir = AppCtx.getInstance().getCacheDir();
     }
 
     public static File getWebCachePath() {
-        return new File(getCachePath(), "webCache");
+        return new File(cacheDir, "webCache");
     }
 }
